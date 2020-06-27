0b225a4d3915c939cdb5710154cab931583100c8
Hajime Suzuki <hajime.doublebass@gmail.com>
2020-06-27 21:51:19 +0200

    get scss file from old projcet

---
+++ src/pdf-template/gulpfile.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+sass.compiler = require('node-sass')
+const pug = require('gulp-pug')
+const browserSync = require('browser-sync').create()
+
+const src = __dirname
+const build = __dirname
+
+const initBrowserSync = () =>
+  browserSync.init({
+    server: build,
+  })
+
+const compileSass = () =>
+  gulp
+    .src(`${src}/*.scss`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(build))
+    .pipe(browserSync.stream())
+
+const compilePug = () =>
+  gulp
+    .src(`${src}/*.pug`)
+    .pipe(pug())
+    .pipe(gulp.dest(build))
+
+gulp.task('default', () => {
+  initBrowserSync()
+  gulp
+    .watch([`${src}/*.scss`, `${src}/*.pug`], gulp.series(compileSass, compilePug))
+    .on('change', browserSync.reload)
+  gulp.watch(`${build}/*.html`).on('change', browserSync.reload)
+})
+
+gulp.task('compile', async () => {
+  gulp.series(compileSass)
+})
